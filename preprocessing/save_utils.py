565c19b9e6d5cfcfc96b4fbf32af503cd585ee35
Dmitry Dzhigaev <ddzhigaev@gmail.com>
2021-01-11 21:08:28 +0100

    Cosmetics in save_utils

--- preprocessing/save_utils.py
+++ preprocessing/save_utils.py
@@ -24,26 +24,3 @@ def save_data_xrf(save_path, data_xrf):
 	data_xrf = np.float32(data_xrf/np.max(data_xrf))
 	imsave(save_path, data_xrf)
 	print(('++ Saved XRF data to %s')%(save_path))
-
-def create_save_path(processing_pre_path, sample_name, scan_number, save_type, ii):
-    try:
-        processing_path = os.path.join(processing_pre_path,("%s_%d_%d")%(sample_name,scan_number[0],scan_number[-1]))
-    except:
-        processing_path = os.path.join(processing_pre_path,("%s_%d")%(sample_name,scan_number[ii]))    
-
-    try:
-        os.mkdir(processing_path)
-        print("++ Processing folder created")
-    except:
-        print("-- Processing folder already exists")
-
-    if save_type == "mat":
-        # Save mat compressed array for matlab                 
-        save_path = os.path.join(processing_path, ("scan_%06d_merlin.mat")%(int(scan_number[ii])))    #The path to save the results
-    elif save_type == "npz":
-        # Save mat compressed array for matlab                 
-        save_path = os.path.join(processing_path, ("scan_%06d_merlin.npz")%(int(scan_number[ii])))    #The path to save the results                        
-    elif save_type == "tif":
-        # Save in the form of tiff file
-        save_path = os.path.join(processing_path, ("scan_%06d_xspress3.tif")%(int(scan_number[ii])))
-    return save_path
