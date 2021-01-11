e275433cbf9d2a8da590b699dac2bfa86022c778
Dmitry Dzhigaev <ddzhigaev@gmail.com>
2021-01-11 21:23:48 +0100

    Cosmetics in save_utils

--- preprocessing/save_utils.py
+++ preprocessing/save_utils.py
@@ -10,17 +10,15 @@ import scipy.io as scio
 import numpy as np
 from tifffile import imsave
 
-def save_data_xrd(save_path, save_type, save_dictionary):
+def save_data(save_path, save_type, save_dictionary):
 	if save_type == "mat":	        	  
 	    scio.savemat(save_path, {'scan': save_dictionary}, do_compression=True, oned_as="column")
 	    print(('++ Saved XRD and meta data to %s')%(save_path))
 	elif save_type == "npz": 
   		np.savez_compressed(save_path, save_dictionary)
   		print(('++ Saved XRD and meta data to %s')%(save_path))
+	elif save_type == 'tif':		
+		imsave(save_path, data_xrf)
+		print(('++ Saved XRF data to %s')%(save_path))	
 	else:
-		error('-- Unrecognized save type! Do nothing ...')
-
-def save_data_xrf(save_path, data_xrf):
-	data_xrf = np.float32(data_xrf/np.max(data_xrf))
-	imsave(save_path, data_xrf)
-	print(('++ Saved XRF data to %s')%(save_path))
+		print('-- Unrecognized save type! Do nothing ...')
